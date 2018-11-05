3af875cda435cd75a8fd95bf59c5e4ad61bb01b7
Behnam Rasooli <ben.rasooli@gmail.com>
2018-11-05 20:31:21 +1100

    Add mongoose library

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ global.__rootdir = __dirname;
 require("dotenv").config();
 const debug = require("debug")("app_startup");
 const express = require("express");
+const mongoose = require("mongoose");
 const morgan = require("morgan");
 
 const app = new express();
 
 if (app.get("env") === "development") app.use(morgan("tiny"));
 app.use(express.json());
+
+mongoose
+  .connect(
+    process.env.DB_CONNECTION_STRING,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    debug("connection stablished!");
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, debug(`Listening on port ${port}...`));
